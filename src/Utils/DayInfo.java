b7e29d43314b72f4a846f2f3d50a58e9119ab23c
ag8 <andrew2000g@gmail.com>
2015-06-26 16:15:42 -0400

    Fixed graph simulation problem
    Still to fix: Getting very large numbers on y-axis.

---
+++ src/Utils/DayInfo.java
@@ -0,0 +1,23 @@
+package Utils;
+
+public class DayInfo {
+    private int day;
+    private float numSick;
+
+    public DayInfo(int day, float numSick) {
+        this.day = day;
+        this.numSick = numSick;
+    }
+
+    public int getDay() {
+        return this.day;
+    }
+
+    public float getNumSick() {
+        return this.numSick;
+    }
+
+    public void setNumSick(float numSick) {
+        this.numSick = numSick;
+    }
+}
--- src/edu/adg/adgsim/Simulator.java
+++ src/edu/adg/adgsim/Simulator.java
@@ -14,8 +14,9 @@ public class Simulator implements Runnable {
     int numberOfPeopleOriginallyInfected = 0;
 
     //Values TODO: Add as parameters
-    int simulationDuration = 100;
+    int simulationDuration = 25;
     int recoveryDays = 5;
+    int simulationNum = 1000;
 
     //State values
     int susceptible = Constants.SUSCEPTIBLE;
@@ -23,7 +24,7 @@ public class Simulator implements Runnable {
     int recovered = Constants.RECOVERED;
 
     //Histories
-    List<List<Day>> histories = new ArrayList<List<Day>>();
+    List<List<DayInfo>> histories = new ArrayList<List<DayInfo>>();
 
     public Simulator(List<Person> people, int numberOfPeopleOriginallyInfected) {
         this.people = people;
@@ -31,10 +32,10 @@ public class Simulator implements Runnable {
     }
 
     public void run() {
-        System.out.println(people.size());
-        for (Person person : people) {
-            System.out.println("Person " + person.getID() + " has " + person.getFriends().size() + " friends.");
-        }
+//        System.out.println(people.size());
+//        for (Person person : people) {
+//            System.out.println("Person " + person.getID() + " has " + person.getFriends().size() + " friends.");
+//        }
 
         //Step 1. Infect the people that should be originally infected.
         for (int i = 0; i < numberOfPeopleOriginallyInfected; i++) {
@@ -46,8 +47,9 @@ public class Simulator implements Runnable {
         }
 
         //Step 2. Simulate with this List of people.
-        for (int i = 0; i < 100; i++) {
-            List<Day> history = simulate(i);
+        for (int i = 0; i < 100; i+=1) {
+            System.out.println("Running simulation for " + i + "%...");
+            List<DayInfo> history = simulate(i);
             histories.add(history);
 
             //Reset people
@@ -80,66 +82,122 @@ public class Simulator implements Runnable {
      * @param infectionConstant - percentage of infecting a friend
      * @return - A history of the simulation
      */
-    private List<Day> simulate(int infectionConstant) {
-        List<Day> days = new ArrayList<Day>();
+    private List<DayInfo> simulate(int infectionConstant) {
+        //A list of all of the simulations
+        List<List<Day>> allDays = new ArrayList<List<Day>>();
+        //A list of all of the days, will be the averages
+        List<DayInfo> dayInfos = new ArrayList<DayInfo>();
+        //Popilate array with zeroes
+        for (int i = 0; i < simulationDuration; i++) {
+            dayInfos.add(new DayInfo(i, 0));
+        }
 
-        while(days.size() < simulationDuration) {
-            //Step 1. Loop through each person, and, if the person is infected, randomly infect non-recovered friends.
-            for (Person p : people) {
-                if (p.getState() == infected) {
-                    for (Person friend : p.getFriends()) {
-                        if (r(infectionConstant) && friend.getState() != recovered) {
-                            friend.setState(infected);
+        for (int i = 0; i < simulationNum; i++) {
+            //Create a list of days for this simulation
+            List<Day> days = new ArrayList<Day>();
+
+            while (days.size() < simulationDuration) {
+                //Step 1. Loop through each person, and, if the person is infected, randomly infect non-recovered friends.
+                for (Person p : people) {
+                    if (p.getState() == infected) {
+                        for (Person friend : p.getFriends()) {
+                            if (r(infectionConstant) && friend.getState() != recovered) {
+                                friend.setState(infected);
+                            }
                         }
                     }
                 }
-            }
 
-            //Step 2. Loop through each person, and, if the person is sick, add a day to his counter.
-            for (Person p : people) {
-                if (p.getState() == infected) {
-                    p.incrementSickDays();
-                }
-            }
-
-            //Step 3. Loop through each person, and, if the person is sick and his sick counter reached the limit, make the person recover.
-            for (Person p : people) {
-                if (p.getState() == infected) {
-                    if (p.getSickDays() == recoveryDays) {
-                        p.resetSickDays();
-                        p.setState(recovered);
+                //Step 2. Loop through each person, and, if the person is sick, add a day to his counter.
+                for (Person p : people) {
+                    if (p.getState() == infected) {
+                        p.incrementSickDays();
                     }
                 }
+
+                //Step 3. Loop through each person, and, if the person is sick and his sick counter reached the limit, make the person recover.
+                for (Person p : people) {
+                    if (p.getState() == infected) {
+                        if (p.getSickDays() == recoveryDays) {
+                            p.resetSickDays();
+                            p.setState(recovered);
+                        }
+                    }
+                }
+
+                //Step 4. Save the people into a day.
+                List<Person> peopleToSave = new ArrayList<Person>();
+                for (Person p : people) {
+                    Person newPerson = new Person(p.getID());
+                    newPerson.setFriends(p.getFriends());
+                    newPerson.setState(p.getState());
+                    peopleToSave.add(newPerson);
+                }
+                days.add(new Day(peopleToSave));
+
+//                System.out.println("SIMULATION " + i + ", DAY " + (days.size()) + ": " + Utils.getNumberOfPeopleSick(people) + " people are sick.");
             }
 
-            //Step 4. Save the people into a day.
-            List<Person> peopleToSave = new ArrayList<Person>();
+            allDays.add(days);
+
+            //Reset people
             for (Person p : people) {
-                Person newPerson = new Person(p.getID());
-                newPerson.setFriends(p.getFriends());
-                newPerson.setState(p.getState());
-                peopleToSave.add(newPerson);
+                p.setState(susceptible);
             }
-            days.add(new Day(peopleToSave));
+
+            //Infect the people that should be originally infected.
+            for (int j = 0; j < numberOfPeopleOriginallyInfected; j++) {
+                Person person = people.get(Utils.randInt(0, people.size() - 1));
+                while (person.getState() == infected) {
+                    person = people.get(Utils.randInt(0, people.size() - 1));
+                }
+                person.setState(infected);
+            }
         }
 
-        return days;
+
+        //Averaging
+
+        //Add all numsicks
+        for (List<Day> dayList : allDays) {
+            for (Day day : dayList) {
+                float numberOfPeopleSickOnThisDay = Utils.getNumberOfPeopleSick(day.getPeople());
+                float sumOfNumbersOfPeopleSickOnThisDay = dayInfos.get(dayList.indexOf(day)).getNumSick();
+                dayInfos.get(dayList.indexOf(day)).setNumSick(numberOfPeopleSickOnThisDay + sumOfNumbersOfPeopleSickOnThisDay);
+            }
+        }
+
+        //Divide by the total number of simulations
+        for (DayInfo dayInfo : dayInfos) {
+            float sumOfNumbersOfPeopleSickOnThisDay = dayInfo.getNumSick();
+            float averageNumberOfPeopleSickOnThisDay = sumOfNumbersOfPeopleSickOnThisDay / dayInfos.size();
+            dayInfo.setNumSick(averageNumberOfPeopleSickOnThisDay);
+        }
+
+
+
+        return dayInfos;
     }
 
     private void createGraph() throws IOException {
         XYSeriesCollection dataset = new XYSeriesCollection();
 
         for (int i = 0; i < histories.size(); i++) {
-            List<Day> history = histories.get(i);
+//            System.out.println("Welcome to History " + i + ".");
+            List<DayInfo> history = histories.get(i);
 
             XYSeries series = new XYSeries(i);
-            series.setDescription("Percentage 1");
 
-            for (Day day : history) {
-                series.add(history.indexOf(day), Utils.getNumberOfPeopleSick(day.getPeople()));
+            series.add(0, numberOfPeopleOriginallyInfected);
+
+            for (DayInfo day : history) {
+//                System.out.println("Welcome to day " + day.getDay() + "! There are " + day.getNumSick() + " people infected.");
+                series.add(day.getDay(), day.getNumSick());
             }
 
             dataset.addSeries(series);
+
+            System.out.println("The maximum number of sick people is " + series.getMaxY() + ".");
         }
 
         Main.dataset = dataset;
